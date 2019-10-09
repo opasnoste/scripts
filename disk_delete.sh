cd0e05bf42624ac7ed4954a45e694c22f5b3f7c4
opasnoste <osorvilov@callfire.com>
2019-10-09 16:03:15 +0300

    Add some fancy stuff to disk_delete script

--- disk_delete.sh
+++ disk_delete.sh
@@ -7,6 +7,19 @@ if [ $# -lt  1 ]
         echo "Usage: $0 name-of-gcloud-project"
 else
 project=$1
+
+echo "Listing all detached disks in human-readable format. 
+Note: The detach timestamp may have a null value. This means that the disk was 
+created, but never attached to an instance."
+
+gcloud compute disks list \
+    --project=$project \
+    --filter='-users:*' \
+    --format='table[box](name,type,sizeGb,creationTimestamp,lastDetachTimestamp)' \
+    --sort-by=lastDetachTimestamp
+
+echo "Starting deletion"
+
 for zone in us-central1-a us-central1-b us-central1-c us-west1-a us-west1-b us-west1-c
     do
         for disk in  $(gcloud compute disks list --project=$project --filter="-users:* AND zone:( $zone )" --format="value(name)")
